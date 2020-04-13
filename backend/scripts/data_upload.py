81a34d48e3fae5e2ba14ac6e7240df37dd997cf4
evanstjabadi <evansltjabadi@gmail.com>
2020-04-13 19:14:56 +0200

    Completed

--- backend/scripts/data_upload.py
+++ backend/scripts/data_upload.py
@@ -9,18 +9,28 @@ BE_URL = "https://remote-monitoring.evanstjabadi.live/backend/randoms/"
 LOCAL_URL = "http://127.0.0.1:5000/randoms/"
 
 
+def data_log(data):
+    with open('data_log.txt', 'a+') as file:
+        file.write(data)
+    return None
+
+
 def data_upload():
     random_value = int(random() * 100)
     time_stamb = datetime.now()
-    print(time_stamb)
+    string_time_stamb = time_stamb.strftime("%Y-%m-%d %H:%M:%S")
 
-    payload = {"value": random_value, "time_stamb": str(time_stamb)}
+    payload = {
+        "value": random_value,
+        "time_stamb": string_time_stamb
+    }
 
     response = requests.post(BE_URL, json=payload)
-    print(response.status_code)
+    log = "Status Code: "+str(response.status_code)+'\n'+"Data: "+str(response.text)+'\n'
+    data_log(log)
 
 
 if __name__ == "__main__":
     while True:
-        sleep(5)  # sleeps for 5 seconds
+        sleep(6)  # sleeps for 5 seconds
         data_upload()
